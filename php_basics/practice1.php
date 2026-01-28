8700cb2581a82d488231e3e41d0fbf750f80447d
SV3105 <snehavaghela3105@gmail.com>
2026-01-28 10:57:41 +0530

    PHP-002-creating associative array and debugging

--- php_basics/practice1.php
+++ php_basics/practice1.php
@@ -7,10 +7,17 @@ $year = 2026;
 echo $greeting . $year;
 $isActive = true;
 
-echo '<br />'
+echo '<br />';
 echo $isActive;
 
 ?>
 
+<?php 
+$person = ["name" => "xyz", "Age" => 34, "Skills" => ["HTML", "CSS", "JS", "PHP"]];
+echo '<br />';
+print_r($person);
+echo '<br />';
+var_dump($person);
+?>
 
 
