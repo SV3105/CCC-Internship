5ffd8408effa990635d42247af41ec6253059e7e
SV3105 <snehavaghela3105@gmail.com>
2026-01-28 14:32:55 +0530

    PHP-008: learn about getter-setter, access modifiers

--- php_basics/practice4.php
+++ php_basics/practice4.php
@@ -8,6 +8,24 @@ class Employee{
         $this->name = $name;
         $this->salary = $salary;
      }
+//PHP-008
+     public function getSalary(){
+        return $this->salary;
+     }
+
+     public function setSalary($amount){
+        if($amount > 0){
+            $this->salary = $amount;
+        }else{
+            echo "invalid salary amount";
+        }
+     }
 }
+
+$obj = new Employee('xyz', 50000);
+ $obj->setSalary(20000);
+ echo $obj->getSalary();
+
+
 
 ?>
