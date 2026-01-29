c640c3c5a55ec1e077592aefd00ca30686715800
SV3105 <snehavaghela3105@gmail.com>
2026-01-29 15:27:00 +0530

    implement static properties and static methods

--- php_basics/practice4_extra.php
+++ php_basics/practice4_extra.php
@@ -72,6 +72,28 @@ $c2->deposit(5000);
 
 echo $c1->getBalance()."<br>";
 echo $c2->getBalance()."<br>";
+echo "<br />"
+?>
 
+<?php 
+// Static properties and methods can be used without creating an instance of the class.
 
+//The static keyword is also used to declare variables in a function which keep their value after the function has ended.
+
+class counter{
+    public static $count = 0;
+    static function increment(){
+       return self::$count++ ;
+    }
+
+    static function getCount(){
+        return self::$count;
+    }
+
+}
+ echo "counter: ". counter::getCount(). "<br>";
+ counter::increment();
+ counter::increment();
+ counter::increment();
+ echo "counter: ". counter::getCount();
 ?>
