8ef525d9526f3c0f2e97fdcec8c4a173921e76b7
SV3105 <snehavaghela3105@gmail.com>
2026-01-30 15:10:12 +0530

    implemented abstract class and method

--- php_basics/practice4/extra/abstract.php
+++ php_basics/practice4/extra/abstract.php
@@ -1,6 +1,29 @@
 <?php 
     abstract class payment{
         protected $amount;
-        
+        function __construct($amount){
+            $this->amount = $amount;
+        }
+
+        abstract public function processPayment();
     }
+
+    class creditCardPayment extends payment{
+       
+       public function processPayment(){
+            return "Processing credit card payment of $this->amount";
+        }
+    }
+
+    class upiPayment extends payment{
+        public function processPayment(){
+            return "Processing UPI payment of $this->amount";
+        }
+    }
+
+    $credit = new creditCardPayment(1200);
+    $upi = new upiPayment(5000);
+
+    echo $credit->processPayment(). "<br />";
+    echo $upi->processPayment();
 ?>
